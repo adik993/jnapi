a38b7968ed9d92d513d3c231121d492d56362c0f
Adrian Skrobacz <adik993@gmail.com>
2017-11-01 19:56:32 +0100

    Example NapiProjekt URL

--- src/main/kotlin/com/adik993/jnapi/providers/NapiProjectSubtitleProvider.kt
+++ src/main/kotlin/com/adik993/jnapi/providers/NapiProjectSubtitleProvider.kt
@@ -8,7 +8,10 @@ import java.util.concurrent.TimeUnit
 class NapiProjectSubtitleProvider : SubtitleProvider {
     val log = loggerFor<NapiProjectSubtitleProvider>()
 
+    val ARCHIVE_PASSWORD = "iBlm8NTigvru0Jr0"
+
     override fun download(hash: String, lang: String): Observable<String> {
+        // GET http://www.napiprojekt.pl/unit_napisy/dl.php?l=ENG&f=dc3cfb9a57309f42f308b5ca3e321e29&t=66fc5&v=other&kolejka=false&nick=&pass=&napios=Linux/UNIX
         log.info("Downloading {} subtitles for file {}", lang, hash)
         return Observable.just("dummy")
                 .delay(10, TimeUnit.SECONDS)
