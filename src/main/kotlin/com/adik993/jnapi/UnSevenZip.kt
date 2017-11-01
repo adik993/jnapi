e99399ff653e824cfff15f0a111abee8451f0f8c
Adrian Skrobacz <adik993@gmail.com>
2017-11-01 23:54:23 +0100

    7z extract improvements

--- src/main/kotlin/com/adik993/jnapi/UnSevenZip.kt
+++ src/main/kotlin/com/adik993/jnapi/UnSevenZip.kt
@@ -1,14 +1,34 @@
 package com.adik993.jnapi
 
 import com.adik993.jnapi.compression.extract7z
+import com.adik993.jnapi.extensions.formatBracets
 import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.ShowHelpException
+import com.xenomachina.argparser.SystemExitException
 import com.xenomachina.argparser.default
 import java.io.File
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     val parser = ArgParser(args)
     val password by parser.storing("-p", "--password", help = "password for the archive").default(null)
     val file by parser.positional("SOURCE", help = "7z archive file path") { File(this) }
-    file.extract7z(file.parentFile, password)
+    val destDir by parser.storing("-o", help = "output directory") { File(this) }.default(null)
+    try {
+        parser.force()
+    } catch (e: ShowHelpException) {
+        val writer = System.out.writer()
+        e.printUserMessage(writer, "un7z", 0)
+        writer.flush()
+        exitProcess(0)
+    } catch (e: SystemExitException) {
+        println(e.message)
+        exitProcess(1)
+    }
+    val dest = destDir ?: file.parentFile
+    println("Extracting files to {} folder...".formatBracets(dest))
+    val extractedFiles = file.extract7z(dest, password)
+    println("Extracted {} files:".formatBracets(extractedFiles.size))
+    extractedFiles.forEach { println("{} ({} bytes)".formatBracets(it.name, it.length())) }
 
 }
--- src/main/kotlin/com/adik993/jnapi/compression/SevenZ.kt
+++ src/main/kotlin/com/adik993/jnapi/compression/SevenZ.kt
@@ -7,16 +7,30 @@ import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.nio.charset.StandardCharsets
+import java.util.*
+import java.util.Spliterator.*
+import java.util.function.Consumer
+import java.util.stream.Stream
+import java.util.stream.StreamSupport
+import kotlin.streams.toList
 
-fun File.extract7z(destDir: File, password: String? = null, bufferSize: Int = 1024) {
+fun File.extract7z(destDir: File, password: String? = null, bufferSize: Int = 1024): List<File> {
     val absoluteDestDir = destDir.toFullPathFile()
-    destDir.mkdirs()
     val archive = SevenZFile(this.toFullPathFile(), password?.toByteArray(StandardCharsets.UTF_16LE))
-    archive.useForEach { entry ->
-        val file = File(absoluteDestDir, entry.name)
-        if (entry.isDirectory) file.mkdirs()
-        else extract7zFile(file.outputStream(), archive, bufferSize)
+    archive.use {
+        return archive.stream()
+                .map { it.createFile(absoluteDestDir) }
+                .filter { !it.isDirectory }
+                .peek { extract7zFile(it.outputStream(), archive, bufferSize) }
+                .toList()
     }
+}
+
+fun SevenZArchiveEntry.createFile(root: File): File {
+    val file = File(root, this.name)
+    if (isDirectory) file.mkdirs()
+    else file.parentFile.mkdirs() && file.createNewFile()
+    return file
 }
 
 internal fun extract7zFile(outputStream: FileOutputStream, archive: SevenZFile, bufferSize: Int) {
@@ -30,14 +44,28 @@ internal fun extract7zFile(outputStream: FileOutputStream, archive: SevenZFile, 
     }
 }
 
-fun SevenZFile.useForEach(block: (SevenZArchiveEntry) -> Unit) {
-    this.use { forEach(block) }
+fun SevenZFile.stream(): Stream<SevenZArchiveEntry> {
+    return StreamSupport.stream(SevenZFileSpliterator(this), false).onClose(this::close)
 }
 
-fun SevenZFile.forEach(block: (SevenZArchiveEntry) -> Unit) {
-    var entry = this.nextEntry
-    while (entry != null) {
-        block(entry)
-        entry = this.nextEntry
+class SevenZFileSpliterator(private val archive: SevenZFile) : Spliterator<SevenZArchiveEntry> {
+
+    override fun tryAdvance(action: Consumer<in SevenZArchiveEntry>?): Boolean {
+        val entry = archive.nextEntry
+        val hasValue = entry != null
+        if (hasValue) action?.accept(entry)
+        return hasValue
+    }
+
+    override fun trySplit(): Spliterator<SevenZArchiveEntry>? {
+        return null
+    }
+
+    override fun estimateSize(): Long {
+        return archive.entries.count().toLong()
+    }
+
+    override fun characteristics(): Int {
+        return ORDERED or IMMUTABLE or NONNULL or SIZED
     }
 }
